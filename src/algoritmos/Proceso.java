085bdc965131d8f5433a6e1fb364bb28ef941a4e
DannyArevaloS <darevalo_dev@outlook.es>
2024-09-30 00:18:56 +0200

    commit nuevo para dir algoritmos

---
+++ src/algoritmos/Proceso.java
@@ -0,0 +1,11 @@
+package algoritmos;
+
+public class Proceso {
+	int id;
+    int tiempoEjecucion;
+
+    public Proceso(int id, int tiempoEjecucion) {
+        this.id = id;
+        this.tiempoEjecucion = tiempoEjecucion;
+    }
+}
---
+++ src/algoritmos/algoritmoShortestJobFirst.java
@@ -0,0 +1,24 @@
+package algoritmos;
+import java.util.*;
+
+
+public class algoritmoShortestJobFirst {
+
+	public static void main(String[] args) {
+        List<Proceso> procesos = new ArrayList<>();
+        procesos.add(new Proceso(1, 3));
+        procesos.add(new Proceso(2, 5));
+        procesos.add(new Proceso(3, 2));
+
+        PriorityQueue<Proceso> colaPrioridad = new PriorityQueue<>(Comparator.comparingInt(p -> p.tiempoEjecucion));
+        colaPrioridad.addAll(procesos);
+
+        int tiempoActual = 0;
+        while (!colaPrioridad.isEmpty()) {
+            Proceso procesoActual = colaPrioridad.poll();
+            System.out.println("Ejecutando proceso " + procesoActual.id + " durante " + procesoActual.tiempoEjecucion + " unidades de tiempo");
+            tiempoActual += procesoActual.tiempoEjecucion;
+        }
+	}
+
+}
